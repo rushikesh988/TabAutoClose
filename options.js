fb8654563892f5f8d98e31f08f37e414f45e59f5
Gabriel Fair <2096785+gabefair@users.noreply.github.com>
2020-11-22 14:09:13 -0500

    add feature to disable notifications

--- options.js
+++ options.js
@@ -2,6 +2,7 @@ function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
     noOfMinutesToAutoClose: document.querySelector("#noOfMinutesToAutoClose").value,
+    isNotificationsEnabled: document.querySelector("#isNotificationsEnabled").value,
     autoStart: document.querySelector("#autoStart").checked
   });
 
@@ -9,6 +10,7 @@ function saveOptions(e) {
 function restoreOptions() {
   function setCurrentChoice(result) {
     document.querySelector("#noOfMinutesToAutoClose").value = result.noOfMinutesToAutoClose || "60";
+    document.querySelector("#isNotificationsEnabled").value = result.isNotificationsEnabled;
     document.querySelector("#autoStart").checked =(((result.autoStart==undefined)? false:true))? result.autoStart :false; 
   }
 
